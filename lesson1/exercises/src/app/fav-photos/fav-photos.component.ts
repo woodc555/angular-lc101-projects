05a0f4cf7bc2b208864fb71587644c4ddaa30800
Christian <wood.c9595@gmail.com>
2021-04-21 22:08:55 -0500

    part 1 of excersice done

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'GOOGEEOHH!';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://static.wikia.nocookie.net/yugioh/images/4/4f/TimeWizard-OW.png/revision/latest?cb=20140622224942';
+  image3 = 'https://uploads2.yugioh.com/card_images/1787/detail/2785.jpg?1385102370';
 
   constructor() { }
 
